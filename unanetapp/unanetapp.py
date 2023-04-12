a2610ae4f0e18c10d6ac5f247703cb2954a7b401
Skycode22 <97566685+Skycode22@users.noreply.github.com>
2023-04-11 22:19:57 -0700

    app done

--- unanetapp/unanetapp.py
+++ unanetapp/unanetapp.py
@@ -4,7 +4,6 @@ import requests
 import os
 from toga import Image
 
-canvas = toga.Canvas()
 box = toga.Box()
 
 def button_handler(widget):
@@ -12,34 +11,17 @@ def button_handler(widget):
     webbrowser.open_new(url)
 
 def build(app):
-    
-
-    button = toga.Button("", on_press=button_handler)
-    button.style.padding = 50
-    button.style.flex = 1
-
-    button.style.color = 'white'
-    button.style.font_family = 'arial'
-    button.style.width = 540
-    button.style.height = 350
-    button.style.font_size = 20
-    button.style.font_weight = 'bold'
-
-    box.add(button)
-
     # Download and save the image locally
-    response = requests.get('https://upload.wikimedia.org/wikipedia/commons/7/7e/Unanet_Logo.png')
+    response = requests.get('https://seeklogo.com/images/U/unanet-logo-C74B018A7C-seeklogo.com.png')
     with open('unanet_logo.png', 'wb') as f:
         f.write(response.content)
-
-    # Create a Toga Image object from the downloaded image file
-    image = Image(os.path.abspath('unanet_logo.png'))
-
-    # Set the background image to the Toga Image object
-    button.style.background_color = 'white'
-    button.style.background_image = image
-    button.style.background_size = 'contain'
-
+    image = Image('unanet_logo.png')
+    button = toga.Button("", on_press=button_handler)
+    button.image = image
+    button.style.padding = 50
+    button.style.width = 540
+    button.style.height = 350
+    box.add(button)
     return box
 
 def main():
