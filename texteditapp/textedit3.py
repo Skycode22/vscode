d42921c27bac12b27668be7d538eecb36fd35931
Skycode22 <97566685+Skycode22@users.noreply.github.com>
2023-04-16 19:37:21 -0700

    Update textedit3.py

--- texteditapp/textedit3.py
+++ texteditapp/textedit3.py
@@ -1,142 +1,59 @@
-import os
-import docx
-import openpyxl
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.popup import Popup
-from kivy.properties import ObjectProperty
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.filechooser import FileChooserIconView
-from kivy.core.window import Window
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
 
-Window.size = (360, 640)
-Window.clearcolor = (.5, 1, 1, 1)
-KV = '''
-#:kivy 2.0.0
-BoxLayout:
-    orientation: "vertical"
-    BoxLayout:
-        size_hint_y: None
-        height: "48dp"
-        Button:
-            text: "Open"
-            on_release: app.open_file()
-        Button:
-            text: "Save"
-            on_release: app.save_file()
-        Button:
-            text: "Save As"
-            on_release: app.save_file_as()        
-    ScrollView:
-        do_scroll_x: False
-        bar_width: "10dp"
-        scroll_type: ['bars']
-        effect_cls: "ScrollEffect"
-        TextInput:
-            id: text_input
-            focus: True
-            font_size: '16sp'
-            size_hint_y: None
-            height: max(self.minimum_height, root.height - 48)
-            on_parent: app.text_input = self
-'''
 
-class TextEditor(App):
-    text_input = ObjectProperty(None)
-    current_file = ''
+class TextEditor(toga.App):
+    def startup(self):
+        # Create a main window
+        self.main_window = toga.MainWindow(title=self.name)
 
-    def build(self):
-        return Builder.load_string(KV)
+        # Create a text area widget
+        self.text_area = toga.MultilineTextInput()
 
-    def open_file(self):
-        filechooser = FileChooserIconView(path=os.getcwd())
-        popup = Popup(title="Open File", content=filechooser, size_hint=(0.8, 0.8))
-        filechooser.bind(on_submit=self.load_file)
-        popup.open()
+        # Create a save button widget
+        self.save_button = toga.Button('Save', on_press=self.save_file)
 
-    def load_file(self, instance, filepaths, *args):
-        if filepaths:
-            self.current_file = filepaths[0]
-            file_ext = os.path.splitext(self.current_file)[1].lower()
-            if file_ext == ".docx":
-                try:
-                    doc = docx.Document(self.current_file)
-                    full_text = []
-                    for paragraph in doc.paragraphs:
-                        if paragraph.text is not None:
-                            full_text.append(paragraph.text)
-                    self.text_input.text = "\n".join(full_text)
-                except Exception as e:
-                    print(f"Error: {e}")
-            elif file_ext == ".xlsx":
-                try:
-                    wb = openpyxl.load_workbook(self.current_file)
-                    ws = wb.active
-                    full_text = []
-                    for row in ws.iter_rows():
-                        row_text = []
-                        for cell in row:
-                            row_text.append(str(cell.value))
-                        full_text.append('\t'.join(row_text))
-                    self.text_input.text = "\n".join(full_text)
-                except Exception as e:
-                    print(f"Error: {e}")
-            else:
-                try:
-                    with open(self.current_file, 'r', encoding='utf-8') as f:
-                        self.text_input.text = f.read()
-                except UnicodeDecodeError as e:
-                    print(f"Error: {e}")
-            instance.parent.parent.parent.dismiss()
-    def save_file(self):
-        if self.current_file:
-            file_ext = os.path.splitext(self.current_file)[1].lower()
-            if file_ext == ".docx":
-                doc = docx.Document()
-                for paragraph_text in self.text_input.text.split("\n"):
-                    doc.add_paragraph(paragraph_text)
-                doc.save(self.current_file)
-            elif file_ext == ".xlsx":
-                wb = openpyxl.Workbook()
-                ws = wb.active
-                for row in self.text_input.text.split("\n"):
-                    row_cells = row.split("\t")
-                    ws.append(row_cells)
-                wb.save(self.current_file)
-            else:
-                with open(self.current_file, 'w', encoding='utf-8') as f:
-                    f.write(self.text_input.text)
-        else:
-            self.save_file_as()
-    def save_file_as(self):
-        filechooser = FileChooserIconView(path=os.getcwd(), dirselect=True)
-        popup = Popup(title="Save File As", content=filechooser, size_hint=(0.8, 0.8))
-        filechooser.bind(on_submit=self.save_new_file_with_popup)
-        popup.open()
-    def save_new_file_with_popup(self, instance, filepaths, *args):
-        if filepaths:
-            filename = os.path.basename(instance.selection[0])
-            self.current_file = os.path.join(filepaths[0], filename)
-            file_ext = os.path.splitext(self.current_file)[1].lower()
-            if file_ext == ".docx":
-                doc = docx.Document()
-                for paragraph_text in self.text_input.text.split("\n"):
-                    doc.add_paragraph(paragraph_text)
-                doc.save(self.current_file)
-            elif file_ext == ".xlsx":
-                wb = openpyxl.Workbook()
-                ws = wb.active
-                for row in self.text_input.text.split("\n"):
-                    row_cells = row.split("\t")
-                    ws.append(row_cells)
-                wb.save(self.current_file)
-            else:
-                with open(self.current_file, 'w', encoding='utf-8') as f:
-                    f.write(self.text_input.text)
-            instance.parent.parent.parent.dismiss()
-class Test(App):
-    def build(self):
-        return KV()
-if __name__ == "__main__":
-    TextEditor().run()
+        # Create an open button widget
+        self.open_button = toga.Button('Open', on_press=self.open_file)
+
+        # Create a horizontal box to hold the text area and buttons
+        box = toga.Box(children=[self.text_area, self.save_button, self.open_button], style=Pack(direction=ROW))
+
+        # Add the box widget to the main window
+        self.main_window.content = box
+
+        # Show the main window
+        self.main_window.show()
+
+    def save_file(self, widget):
+        # Get the text from the text area widget
+        text = self.text_area.value
+
+        # Open a file dialog to choose a file to save
+        file_path = self.main_window.save_file_dialog()
+
+        # Write the text to the file
+        with open(file_path, 'w') as f:
+            f.write(text)
+
+    def open_file(self, widget):
+        # Open a file dialog to choose a file to open
+        file_path = self.main_window.open_file_dialog(title="Open File")
+
+        # Check if a valid file path was returned
+        if file_path:
+            # Read the contents of the file
+            with open(file_path, 'r') as f:
+                text = f.read()
+
+            # Set the text area widget value to the contents of the file
+            self.text_area.value = text
+
+
+def main():
+    return TextEditor('Text Editor', 'com.example.texteditor')
+
+
+if __name__ == '__main__':
+    main().main_loop()
